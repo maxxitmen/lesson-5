adc1661afdfb4c9dc6ee213dfc248d461a231b35
maxxitmen <61408791+maxxitmen@users.noreply.github.com>
2024-04-21 20:42:32 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 3, 'Maxim', True, 1.25)
+print(immutable_var)
+# immutable_var[3] = 'Vlad' изменить элемент нельзя
+mutable_list = [1, 1, 2, 3, 5, 8, 13, 'a', 'b']
+mutable_list[3] = 100
+print(mutable_list)
